4dc1816a31d5d8bf154edf4d46e79d09eb5af01a
AlaaSherif55 <alaa.sherif.fathi@gmail.com>
2024-04-08 00:47:36 +0200

    add lab1 for vue

---
+++ LAB1/BookStore/books.js
@@ -0,0 +1,65 @@
+books = [
+  {
+    isbn: "9780451160522",
+    name: "To Kill a Mockingbird",
+    category: "Fiction",
+    image: "https://marketplace.canva.com/EAFaQMYuZbo/1/0/1003w/canva-brown-rusty-mystery-novel-book-cover-hG1QhA7BiBU.jpg",
+    pages: 281,
+    author: "Harper Lee",
+    price: 9.99
+  },
+  {
+    isbn: "9780140283334",
+    name: "1984",
+    category: "Fiction",
+    image: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/a-beautiful-unique-book-cover-art-design-template-0d50ce167d52e19534fe3af0307ff273_screen.jpg?ts=1637000940",
+    pages: 328,
+    author: "George Orwell",
+    price: 11.99
+  },
+  {
+    isbn: "9780061120084",
+    name: "The Catcher in the Rye",
+    category: "Fiction",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGLpBIhkp_KTs_j2w9xMJhr6WDTkG3Aef883RT2Wxe8ol5MPqTUqEbI0E4W7LB9k2goFA&usqp=CAU",
+    pages: 277,
+    author: "J.D. Salinger",
+    price: 10.49
+  },
+  {
+    isbn: "9780743273565",
+    name: "The Great Gatsby",
+    category: "Fiction",
+    image: "https://static-cse.canva.com/blob/921489/RedandBeigeCuteIllustrationYoungAdultBookCover.jpg",
+    pages: 180,
+    author: "F. Scott Fitzgerald",
+    price: 4.3
+  },
+  {
+    isbn: "9780743273565",
+    name: "The Hobbit",
+    category: "Fantasy",
+    image: "https://cdn.vox-cdn.com/thumbor/p-gGrwlaU4rLikEAgYhupMUhIJc=/0x0:1650x2475/1200x0/filters:focal(0x0:1650x2475):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/13757614/817BsplxI9L.jpg",
+    pages: 310,
+    author: "J.R.R. Tolkien",
+    price: 12.25
+  },
+  {
+    isbn: "9780812416801",
+    name: "The Grapes of Wrath",
+    category: "Fiction",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWXiMfxZehLYO--wDCarz15mtKKuvRGXzHQWdMcgM9wA&s",
+    pages: 464,
+    author: "John Steinbeck",
+    price: 2.75
+  },
+  {
+    isbn: "9780439358071",
+    name: "Harry Potter and the Philosopher's Stone",
+    category: "Fantasy",
+    image: "https://m.media-amazon.com/images/I/81q77Q39nEL._AC_UF1000,1000_QL80_.jpg",
+    pages: 309,
+    author: "J.K. Rowling",
+    price: 14.99
+  }
+];
